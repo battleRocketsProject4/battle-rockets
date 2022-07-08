19f63f24fc7e6eaf14ff34c576acaa9087fc9a17
Lauryn Ferkul <laurynferkul4@gmail.com>
2022-07-07 22:59:21 -0400

    cleaned up react file, added some assets & put some base styles in

--- src/Form.js
+++ src/Form.js
@@ -1,16 +1,13 @@
 const Form = () => {
   return (
     <form action='submit'>
-      <label htmlFor='playerName'>astronauts enter your name</label>
+      <label htmlFor='playerName'>Astronauts, enter your name</label>
       <div className='inputContainer'>
         <input type='text' id='playerName' placeholder='Astronaut 1' />
         <input type='text' id='playerName' placeholder='Astronaut 2' />
       </div>
       <p>
-        never played Battle Rockets before?
-        <span>
-          <a href='#'>click here</a>
-        </span>
+        Never played Battle Rockets before? <a href='#'>Click here</a>
       </p>
       <button>take off!</button>
     </form>
